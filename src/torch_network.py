ede19daaa732a95edf07d27ec1f325a4ce1a1a2e
OmSa-20240212 <159829357+OmSa120224@users.noreply.github.com>
2025-09-19 15:17:46 +0200

    saving

--- src/torch_network.py
+++ src/torch_network.py
@@ -5,8 +5,8 @@ import torch.nn as nn
 class Network(nn.Module):
     """Thid is a network class"""
 
-    def __init__(self, sizes):
-        super(Network, self).__init__()
+    def __init__(self, sizes: list[int]):
+        super().__init__()
         self.num_layers = len(sizes)
         self.sizes = sizes
 
@@ -18,10 +18,19 @@ class Network(nn.Module):
             [nn.Parameter(torch.randn(y, x)) for x, y in zip(sizes[:-1], sizes[1:])]
         )
 
+    def forward(self, x):
+        # x should be of shape (input_dim, batch_size)
+        for w, b in zip(self.weights[:-1], self.biases[:-1]):
+            x = torch.relu(torch.matmul(w, x) + b)
+        x = torch.matmul(self.weights[-1], x) + self.biases[-1]
+        return x
+
 
 # ------------------------------ Testing what the code does --------------------------- #
 if __name__ == "__main__":
     size = [3, 5, 2]
     net = Network(size)
-    print("Baises = ", net.biases)
-    print("Wheights = ", net.weights)
+    # Print weights and biases more clearly
+    for i, (w, b) in enumerate(zip(net.weights, net.biases), 1):
+        print(f"Layer {i} weights:\n{w.data}")
+        print(f"Layer {i} biases:\n{b.data}\n")
