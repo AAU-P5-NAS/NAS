419532df887c753c4e3a8612cda7a6fc07c29c83
Lukas Saltenis <lsalte23@student.aau.dk>
2025-09-22 00:12:33 +0200

    Neural network training using Pytorch implemented

--- src/torch_network.py
+++ src/torch_network.py
@@ -5,32 +5,15 @@ import torch.nn as nn
 class Network(nn.Module):
     """Thid is a network class"""
 
-    def __init__(self, sizes: list[int]):
+    def __init__(self, architecture: nn.Sequential):
         super().__init__()
-        self.num_layers = len(sizes)
-        self.sizes = sizes
+        self.flatten = nn.Flatten()
+        self.architecture = architecture
 
-        # Create biases as nn.Parameter tensors
-        self.biases = nn.ParameterList([nn.Parameter(torch.randn(y, 1)) for y in sizes[1:]])
-
-        # Create weights as nn.Parameter tensors
-        self.weights = nn.ParameterList(
-            [nn.Parameter(torch.randn(y, x)) for x, y in zip(sizes[:-1], sizes[1:])]
-        )
-
-    def forward(self, x):
-        # x should be of shape (input_dim, batch_size)
-        for w, b in zip(self.weights[:-1], self.biases[:-1]):
-            x = torch.relu(torch.matmul(w, x) + b)
-        x = torch.matmul(self.weights[-1], x) + self.biases[-1]
-        return x
+    def forward(self, data: torch.Tensor):
+        flattened_data = self.flatten(data)
+        return self.architecture(flattened_data)
 
 
 # ------------------------------ Testing what the code does --------------------------- #
-if __name__ == "__main__":
-    size = [3, 5, 2]
-    net = Network(size)
-    # Print weights and biases more clearly
-    for i, (w, b) in enumerate(zip(net.weights, net.biases), 1):
-        print(f"Layer {i} weights:\n{w.data}")
-        print(f"Layer {i} biases:\n{b.data}\n")
+# if __name__ == "__main__":
--- src/train.py
+++ src/train.py
@@ -1,10 +1,51 @@
 from utils.ffn_utils import make_ffn, export_ffn_to_onnx
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
+from torch.nn.modules.loss import _Loss
+from torch.optim.optimizer import Optimizer
+from typing import cast
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # how too build a FFN
-ffn = make_ffn([
-    (10, 20, "relu"),
-    (20, 5, "softmax")
-])
+ffn = make_ffn([(28 * 28, 100, "relu"), (100, 10, "softmax")])
 
 # How too export this to a ONNX file
-export_ffn_to_onnx(ffn, input_size=10, filename="fnn.onnx")
+export_ffn_to_onnx(ffn, input_size=28 * 28, filename="fnn.onnx")
+
+# ----- Training network -----
+
+
+def train(dataloader: DataLoader, model: nn.Module, loss_function: _Loss, optimizer: Optimizer):
+    model.train()
+    for X, y in dataloader:
+        X = X.to(device)
+        y = y.to(device)
+
+        # Compute prediction error
+        predictions = model(X)
+        loss = loss_function(predictions, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+def test(dataloader: DataLoader, model: nn.Module, loss_function: _Loss):
+    model.eval()
+    test_loss: float = 0
+    correct: int = 0
+    cardinality: int = len(cast(TensorDataset, dataloader.dataset))
+    with torch.no_grad():
+        for X, y in dataloader:
+            X = X.to(device)
+            y = y.to(device)
+            predictions = model(X)
+            test_loss += loss_function(predictions, y).item()
+            correct += (predictions.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= len(dataloader)
+    accuracy = correct / cardinality
+
+    return (accuracy, test_loss)
