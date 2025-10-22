fde5c8f9f98219788a25bc11c360ba59e990096f
Lynneruup <76641014+Lynneruup@users.noreply.github.com>
2025-10-22 11:31:44 +0200

    testing

---
+++ train_example_arch.py
@@ -0,0 +1,39 @@
+from src.classification_module.train import Trainer
+from src.data_module.importer import DataImporter, DatasetOption
+import torch
+
+
+def main():
+    importer = DataImporter(dataset_option=DatasetOption.EMNIST_LETTERS)
+    dataloaders = importer.get_as_cnn(batch_size=64, test_split=0.2)
+    number_of_classes = 26
+    pre_model = [
+        torch.nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=0),
+        torch.nn.MaxPool2d(kernel_size=2, stride=2),
+        torch.nn.Flatten(),
+        torch.nn.Linear(32 * 13 * 13, 512),
+        torch.nn.ReLU(),
+        torch.nn.Linear(512, 128),
+        torch.nn.ReLU(),
+        torch.nn.Linear(128, number_of_classes),
+        torch.nn.Softmax(dim=1),
+    ]
+    model = torch.nn.Sequential(
+        *pre_model,
+    )
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    trainer = Trainer(
+        dataloaders=dataloaders,
+        model=model,
+        optimizer=optimizer,
+        loss_function=torch.nn.CrossEntropyLoss(),
+    )
+    for epoch in range(200):
+        trainer.train()
+        metrics = trainer.test()
+
+        print(f"Test Metrics: {metrics}")
+
+
+if __name__ == "__main__":
+    main()
