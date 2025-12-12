bffb18230c83412e6c42ea1d5c1c7c72f0b27c65
EmilVorre <evorre23@student.aau.dk>
2025-12-12 14:09:01 +0100

    training complete

--- test_top3.py
+++ test_top3.py
@@ -1,23 +1,14 @@
-import traceback
-import optuna
 import torch
-from typing import Dict, Any, Optional
 from rich.console import Console
 
-from src.environment.reward.reward import Weights
-from src.environment.reward.weighted_sum import WeightedSumRS
 from src.environment.train import Trainer
 
 from torch import nn
 from src.environment.metrics import Evaluator
 from src.utils.data_importer.dataset import DatasetOption
-from src.utils.hyperparameters import HyperparameterSearchSpace
 from src.utils.data_importer.importer import DataImporter
 
-from src.environment.environment import Trainer
 from torch.optim import AdamW
-from src.environment.metrics import Evaluator
-from src.utils.data_importer.importer import DataImporter
 
 console = Console()
 
@@ -39,6 +30,10 @@ def main():
         nn.BatchNorm2d(128),
         nn.Conv2d(128, 64, kernel_size=5, stride=1, padding=2),
         nn.BatchNorm2d(64),
+        nn.AdaptiveAvgPool2d((1, 1)),
+        nn.Flatten(),
+        nn.Dropout(0.2),
+        nn.Linear(64, 10),
     )
     model[1] = nn.Sequential(
         nn.Conv2d(3, 128, kernel_size=3, stride=1, padding=1),
@@ -55,6 +50,10 @@ def main():
         nn.BatchNorm2d(128),
         nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
         nn.BatchNorm2d(64),
+        nn.AdaptiveAvgPool2d((1, 1)),
+        nn.Flatten(),
+        nn.Dropout(0.2),
+        nn.Linear(64, 10),
     )
     model[2] = nn.Sequential(
         nn.Conv2d(3, 128, kernel_size=3, stride=1, padding=1),
@@ -71,12 +70,16 @@ def main():
         nn.BatchNorm2d(128),
         nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
         nn.BatchNorm2d(128),
+        nn.AdaptiveAvgPool2d((1, 1)),
+        nn.Flatten(),
+        nn.Dropout(0.2),
+        nn.Linear(128, 10),
     )
-
+    metrics = [None, None, None]
     importer = DataImporter(dataset_option=DatasetOption.CIFAR_10)
     trainer = Trainer(
         dataloaders=importer.get_dataloaders(batch_size=64),
-        loss_function=torch.nn.CrossEntropyLoss()
+        loss_function=torch.nn.CrossEntropyLoss(),
     )
     evaluator = Evaluator(
         num_classes=10,
@@ -85,15 +88,17 @@ def main():
         device=torch.device("cuda"),
         loss_function=torch.nn.CrossEntropyLoss(),
     )
-    optimizer = AdamW(model.parameters(), 0.00132)
+    optimizer = AdamW(model[0].parameters(), 0.00132)
     for i in range(3):
-        print(f"training arch {i+1}")
+        print(f"training arch {i + 1}")
         for epoch in range(75):
+            print("Epoch:", epoch + 1)
             trainer.train(model[i], optimizer)
         metrics[i] = evaluator.evaluate(model[i])
         console.print(
-            f"[bold green]Metrics for arch {i+1}: Accuracy: {metrics.accuracy:.4f}, FLOPS: {metrics.flops:.2f}[/bold green]"
+            f"[bold green]Metrics for arch {i + 1}: Accuracy: {metrics[i].accuracy:.4f}, FLOPS: {metrics[i].flops:.2f}[/bold green]"
         )
-       
+
+
 if __name__ == "__main__":
     main()
